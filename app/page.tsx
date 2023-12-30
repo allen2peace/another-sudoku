c7c8b211f2f438ed5e1b693b68a43497c49927a5
caobainan <allencao@dofun.cn>
2023-12-30 15:03:56 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
+"use client"
 import Image from 'next/image'
+import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import { DifficultyLevel, Board } from "./constants and types/types";
+
+import {
+  prepareBoard,
+  isBoardCorrectlyCompleted,
+  findEmpty,
+} from "./logic/functions";
+import { useTimer } from "./hooks/useTimer";
+import Stopwatch from "./components/StopWatch";
+import Rows from "./components/Rows";
+import PauseModal from "./components/PauseModal";
+import GameOverModal from "./components/GameOverModal";
+import { exampleSudokuSolution } from "./constants and types/constants";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  
+  const [board, setBoard] = useState(() =>
+  prepareBoard(exampleSudokuSolution, "easy")
+);
+const [paused, setPaused] = useState(false);
+const [editableFields, setEditableFields] = useState(() => findEmpty(board));
+const timer = useTimer();
+const [difficultyLevel, setDifficultyLevel] =
+  useState<DifficultyLevel>("easy");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+const boardCompleted = useMemo(
+  () => isBoardCorrectlyCompleted(board),
+  [board]
+);
+
+useEffect(() => {
+  if (boardCompleted) {
+    timer.pauseTimer();
+  }
+}, [board]);
+
+const isCellEditable = (index: number): boolean =>
+  editableFields.includes(index);
+
+const handleChange = (index: number, value: string): void => {
+  const copy = [...board];
+  const parsedValue: number = parseInt(value);
+
+  copy[index] = isNaN(parsedValue) ? "" : parsedValue;
+
+  setBoard(copy);
+};
+
+const changeDifficulty = (
+  board: Board,
+  exampleSudokuSolution: number[],
+  level
+): ("" | number)[] => {
+  let boardWithChangedDifficultyLevel: ("" | number)[] = prepareBoard(
+    exampleSudokuSolution,
+    level
+  );
+  setBoard(boardWithChangedDifficultyLevel);
+  setEditableFields(findEmpty(boardWithChangedDifficultyLevel));
+  timer.reset();
+  handleStart();
+  return board;
+};
+
+const handleNewGame = () =>
+  changeDifficulty(board, exampleSudokuSolution, difficultyLevel);
+
+const handleStart = (): void => {
+  timer.startTimer();
+  setPaused(false);
+};
+
+const handlePause = (): void => {
+  timer.pauseTimer();
+  setPaused(true);
+};
+
+const handleChangeDifficulty = (difficulty: DifficultyLevel) => {
+  changeDifficulty(board, exampleSudokuSolution, difficulty);
+  setDifficultyLevel(difficulty);
+};
+
+return (
+  
+  <>
+    <div className="py-[15vh] sm:py-[20vh] container flex flex-col items-center justify-center" >
+      <h1 className="header">Sudoku Game</h1>
+      <div className="difficultyLevelAndTimerContainer">
+        <div className="difficultyLevel">
+          <label>Difficulty level: </label>
+          <select className="select"
+            onChange={(e) => {
+              handleChangeDifficulty(e.target.value);
+            }}
+          >
+            <option value="easy">Easy </option>
+            <option value="medium">Medium </option>
+            <option value="hard">Hard </option>
+          </select>
+        </div>
+        <Stopwatch
+          onPause={handlePause}
+          onStart={handleStart}
+          time={timer.time}
+          isActive={timer.isActive}
+          boardCompleted={boardCompleted}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="board">
+      {boardCompleted && (
+      <GameOverModal onClick={handleNewGame} solutionTime={timer.time} />
+    )}
+        {boardCompleted && <div className="overlay"></div>}
+        {paused && <PauseModal onStart={handleStart} />}
+        <table>
+          <tbody>
+            <Rows
+              board={board}
+              onChange={handleChange}
+              isCellEditable={isCellEditable}
+            />
+          </tbody>
+        </table>
       </div>
-    </main>
-  )
+    </div>
+   
+  </>
+);
 }
